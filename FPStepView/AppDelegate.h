f23286f6c7af8582484e9874deb8a7521b2854b8
Liu Yi <61upup@gmail.com>
2015-06-07 10:19:09 +0800

    Initial Commit

---
+++ FPStepView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FPStepView
+//
+//  Created by im61 on 6/7/15.
+//  Copyright (c) 2015 Fellow Plus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FPStepView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FPStepView
+//
+//  Created by im61 on 6/7/15.
+//  Copyright (c) 2015 Fellow Plus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
